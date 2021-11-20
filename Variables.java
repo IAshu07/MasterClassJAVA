9103b5b06afc5efe1dbbe06bf26e5ac9adac3162
= <=>
2021-11-20 18:01:47 +0530

    Variables in JAVA

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables{
+
+	public static void main(String[] args) {
+
+		int myFirstNumber = (7*2) + (7+2);
+		int mySecondNumber = 8;
+		int myThirdNumber = myFirstNumber + 10;
+		int myTotalNumber = myFirstNumber + mySecondNumber + myThirdNumber;
+		System.out.println(myTotalNumber);
+		int myLastOne = 1000 - myFirstNumber;
+		System.out.println(myLastOne);
+		
+	}
+} 
